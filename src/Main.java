4fa485848ea6fc6b86988d033ec4b667a0197bfe
Valentin <valentinnitu500@gmail.com>
2025-10-23 22:28:38 +0200

    Versión inicial del programa HolaMundoGitHub

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Programa HolaMundoGitHub
+ * Muestra un mensaje de saludo por pantalla.
+ * Autor: [Valentin Eduard Nitu]
+ * Fecha: [23/10/2025]
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, GitHub! Este es mi primer repositorio en Java.");
+    }
+}
