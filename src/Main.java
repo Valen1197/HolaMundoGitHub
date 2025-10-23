003eebe76468b18e46631012ab35d25ab9d2b20e
Valentin <valentinnitu500@gmail.com>
2025-10-23 22:38:45 +0200

    Version 2

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("¡Hola, GitHub! Este es mi primer repositorio en Java.");
+        System.out.println("Soy Valentin");
     }
 }
